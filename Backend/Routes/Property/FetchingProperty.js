592b1aca086fcfed80e1d25bd68007cdd21e74c1
linguisticLevi21 <amishaadi0102@gmail.com>
2026-02-07 15:25:41 +0530

    second commit

---
+++ Backend/Routes/Property/FetchingProperty.js
@@ -0,0 +1,82 @@
+import express from "express";
+import { FindProperty, FindOneProperty, FindingProperties, FindValidatedStaleProperties } from "../../Database/Property/Get/FindingProperty.js";
+import { FindRole, getAuthUser } from "../../Middleware/Middleware.js";
+
+const router = express.Router();
+
+/* Get all Status Based Properties */
+router.get("/properties", async (req, res) => {
+  try {
+    const { status, listed } = req.query;
+    if (!status || listed === undefined) {
+      return res.status(400).json({
+        error: "status and listed query params are required"
+      });
+    }
+    const properties = await FindProperty(
+      status,
+      listed === "true"
+    );
+    return res.status(200).json(properties);
+
+  } catch (err) {
+    console.error("Error fetching properties:", err.message);
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+/* Get Each Property Status Based */
+router.get("/properties/:id", async (req, res) => {
+  try {
+    const { id: propertyId } = req.params;
+    const { status, listed } = req.query;
+    if (!status || listed === undefined) {
+      return res.status(400).json({
+        error: "status and listed query params are required"
+      });
+    }
+    const property = await FindOneProperty(
+      propertyId,
+      status,
+      listed === "true"
+    );
+    return res.status(200).json(property);
+
+  } catch (err) {
+    console.error("Error fetching property:", err.message);
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+/* Get Each User Properties */
+router.get("/userproperties", async (req, res) => {
+  try {
+    const user = await getAuthUser(req);
+    const property = await FindingProperties(user.id);
+    return res.status(200).json(property);
+  } catch (err) {
+    console.error("Error fetching properties:", err.message);
+    return res.status(400).json({ error: err.message });
+  }
+}); 
+
+
+router.get("/warnedproperties", async (req, res) => {
+  try {
+    const user = await getAuthUser(req);
+    if (!user) return res.status(401).json({ error: "Unauthorized" });
+    const role = await FindRole(user.id);
+    if (role !== "Admin") {
+      return res.status(403).json({ error: "Forbidden" });
+    }
+    const properties = await FindValidatedStaleProperties(2); 
+    return res.status(200).json(properties);
+  } catch (err) {
+    console.error("Error fetching properties:", err);
+    return res.status(400).json({ error: err.message || "Failed to fetch properties" });
+  }
+}); 
+ 
+
+
+export default router;
---
+++ Backend/Routes/Property/UpdatingProperty.js
@@ -0,0 +1,110 @@
+import express from "express";
+import AddProperty from "../../Database/Property/Post/AddProperty.js";
+import { FindRole, getAuthUser, upload } from "../../Middleware/Middleware.js";
+import ValidateProperty from "../../Database/Property/Post/ValidateProperty.js";
+import { FreezeProperty, WarnProperty } from "../../Database/Property/Post/ReviewProperty.js";
+
+const router = express.Router();
+
+/* Add Property */
+router.post("/property/add", upload.fields([
+  { name: "propertyImages", maxCount: 10 },
+  { name: "legalDocuments", maxCount: 10 }
+]), async (req, res) => {
+  try {
+    const user = await getAuthUser(req);
+    const propertyData = req.body;
+    const files = req.files;
+    await AddProperty(propertyData, user, files);
+    return res.status(201).json({
+      message: "Property added successfully"
+    });
+  } catch (err) {
+    console.error("Error Adding Property:", err.message);
+    return res.status(400).json({
+      error: err.message
+    });
+  }
+});
+
+/* Validate Property */
+router.put("/property/validate", async (req, res) => {
+  try {
+    const user = await getAuthUser(req); 
+    const role = await FindRole(user.id); 
+    if (role != "Admin") {
+      return res.status(403).json({
+        error: "Forbidden"
+      });
+    }
+    const propertyData = req.body;
+    const property = await ValidateProperty(propertyData, user);
+    return res.status(200).json({
+      message: "Property validated successfully",
+      property
+    });
+
+  } catch (err) {
+    console.error("Error validating property:", err.message);
+
+    return res.status(400).json({
+      error: err.message
+    });
+  }
+});
+
+
+
+router.put("/property/warn", async (req, res) => {
+  try {
+    const user = await getAuthUser(req); 
+    const role = await FindRole(user.id); 
+    if (role != "Admin") {
+      return res.status(403).json({
+        error: "Forbidden"
+      });
+    }
+    const propertyData = req.body;
+    const property = await WarnProperty(propertyData);
+    return res.status(200).json({
+      message: "Warning Sent Successfully",
+      property
+    });
+
+  } catch (err) {
+    console.error("Error validating property:", err.message);
+
+    return res.status(400).json({
+      error: err.message
+    });
+  }
+});
+
+
+router.put("/property/freeze", async (req, res) => {
+  try {
+    const user = await getAuthUser(req); 
+    const role = await FindRole(user.id); 
+    if (role != "Admin") {
+      return res.status(403).json({
+        error: "Forbidden"
+      });
+    }
+    const propertyData = req.body;
+    const property = await FreezeProperty(propertyData);
+    return res.status(200).json({
+      message: "Warning Sent Successfully",
+      property
+    });
+
+  } catch (err) {
+    console.error("Error validating property:", err.message);
+
+    return res.status(400).json({
+      error: err.message
+    });
+  }
+});
+
+
+export default router;
